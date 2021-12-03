711c82bf9ff1bc232d07ea25da90c28e45c805bc
JoakimDr <56847560+JoakimDr@users.noreply.github.com>
2021-12-03 14:17:24 +0100

    Add files via upload

--- day1/day1.js
+++ day1/day1.js
@@ -1,3 +1,4 @@
+const { sign } = require("crypto");
 var fs = require("fs");
 
 var inputData = [];
@@ -10,12 +11,36 @@ try {
 }
 
 var anotherCount = 0;
+console.log(`numbert of elements found: ${inputData.length.toString()}`);
+console.log(`Last element: ${inputData[inputData.length - 2]}`);
 
 var oldDepth = 0;
-for (let x = 1; x < inputData.length; ++x) {
-  if (inputData[x] >= inputData[x - 1]) {
+for (let x = 1; x < inputData.length - 1; x++) {
+  if (inputData[x] - inputData[x - 1] > 0) {
     anotherCount++;
   }
 }
 
-console.log(anotherCount);
+console.log(`part 1 answer: ${anotherCount}`);
+
+// part 2
+var oldSum = 0;
+var sum = 0;
+var sumIncreases = 0;
+var x = 2;
+
+while (inputData.length - x > 2) {
+  sum =
+    parseInt(inputData[x]) +
+    parseInt(inputData[x - 1]) +
+    parseInt(inputData[x - 2]);
+  console.log(`sum ${sum} old sum ${oldSum}`);
+  if (sum - oldSum > 0) {
+    sumIncreases++;
+  }
+  console.log(sumIncreases);
+  oldSum = sum;
+  x++;
+}
+
+console.log(`Part 2 answer: ${sumIncreases}`);
