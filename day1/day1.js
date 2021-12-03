d575717f7192bf907e52c85e6df3be16a7d67531
JoakimDr <56847560+JoakimDr@users.noreply.github.com>
2021-12-03 14:34:27 +0100

    Add files via upload

--- day1/day1.js
+++ day1/day1.js
@@ -11,12 +11,10 @@ try {
 }
 
 var anotherCount = 0;
-console.log(`numbert of elements found: ${inputData.length.toString()}`);
-console.log(`Last element: ${inputData[inputData.length - 2]}`);
 
 var oldDepth = 0;
 for (let x = 1; x < inputData.length - 1; x++) {
-  if (inputData[x] - inputData[x - 1] > 0) {
+  if (parseInt(inputData[x]) > parseInt(inputData[x - 1])) {
     anotherCount++;
   }
 }
@@ -34,11 +32,9 @@ while (inputData.length - x > 2) {
     parseInt(inputData[x]) +
     parseInt(inputData[x - 1]) +
     parseInt(inputData[x - 2]);
-  console.log(`sum ${sum} old sum ${oldSum}`);
   if (sum - oldSum > 0) {
     sumIncreases++;
   }
-  console.log(sumIncreases);
   oldSum = sum;
   x++;
 }
